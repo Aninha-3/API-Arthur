377e94100d9a92bd496dfe7a060f010450e4c546
Aninha-03 <anacbrito2007@gmail.com>
2025-05-06 16:14:51 -0300

    Adicionando uma pasta e um arquivo de Middleware

--- controllers/userController.js
+++ controllers/userController.js
@@ -3,7 +3,7 @@ const User = require("../model/user");
 
 //importar o bcrypt para criptografar a senha
 const bcrypt = require("bcryptjs");
- 
+
 const userController = {
 
     //criar login 
@@ -19,7 +19,7 @@ const userController = {
     create: async (request, response) => {
         try {
             const { nome, email, senha } = request.body;
- 
+
             if (!nome || !email || !senha) {
                 return response.status(400).json({
                     msg: "Campo incorreto ou vazio"
@@ -33,9 +33,9 @@ const userController = {
                     email: email
                 }
             });
-              
+
             // Adequado ser os dois iguais
-            
+
             if (userEncontrado) {
                 return response.status(400).json({
                     msg: "Usuário ou Senha incorretos"
@@ -60,20 +60,23 @@ const userController = {
             // Token vai sobreviver por 1h
             // palavra secreta para criar o token  -> lady_gltter_divonica
             // bGFkeV9nbHR0ZXJfZGl2b25pY2E=
-             const token = jwt.sign(payload, palavraSecreta, {
+            const token = jwt.sign(payload, palavraSecreta, {
                 expiresIn: "1h"
-             });
+            });
+
+
+
+
+            return response.status(201).json({
+                msg: " O usuário foi crido com sucesso",
+                userCriado
+            })
 
 
             // senha criptografada
             const hashedSenha = await bcrypt.hash(senha, 10);
             const userCriado = await User.create({ nome, email, senha: hashedSenha });
- 
-            return response.status(201).json({
-                msg:" O usuário foi crido com sucesso",
-                userCriado
-            })
-             
+
         } catch (error) {
             console.log(error)
             return response.status(500).json({
@@ -86,20 +89,20 @@ const userController = {
         try {
             const { id } = request.params;
             const { nome, email, senha } = request.body;
- 
-            if(!nome || !email || !senha) {
+
+            if (!nome || !email || !senha) {
                 return response.status(400).json({
                     msg: "Campo inconrreto ou vazio"
                 });
             }
- 
+
             const userExiste = await User.findByPk(id);
-            if(!userExiste) {
+            if (!userExiste) {
                 return response.status(400).json({
                     msg: "Usuário não encontrado"
                 });
             }
- 
+
             await User.update({
                 nome, email, senha
             }, {
@@ -112,11 +115,11 @@ const userController = {
                 msg: " Ocorreu um erro ao acessar a API"
             })
         }
-     },
+    },
     findAll: async (request, response) => {
         try {
             const user = await User.findAll()
- 
+
             return response.status(201).json(user)
         } catch (erro) {
             return response.status(500).json({
@@ -128,8 +131,8 @@ const userController = {
         try {
             const { id } = request.params;
             const existeUser = await User.findByPk(id)
-     
- 
+
+
             if (!existeUser) {
                 return response.status(400).json({
                     msg: "Usuário não foi encontrado"
@@ -143,7 +146,7 @@ const userController = {
             return response.status(200).json({
                 msg: " O usuário foi deletado com sucesso"
             })
- 
+
         } catch (erro) {
             return response.status(500).json({
                 msg: " Ocorreu um erro ao deletar o usuário"
@@ -152,19 +155,19 @@ const userController = {
     },
     findById: async (request, response) => {
         try {
-           
+
             const { id } = request.params;
- 
+
             const userEncontrado = await User.findByPk(id);
- 
+
             if (!existeUser) {
-             
+
                 return response.status(204).json({
                     msg: "Usuário não foi encontrado"
                 })
             }
             return response.status(200).json(userEncontrado)
-             
+
         } catch (error) {
             return response.status(500).json({
                 msg: " Ocorreu um erro ao deletar o usuário"
@@ -172,5 +175,5 @@ const userController = {
         }
     }
 }
- 
+
 module.exports = userController;
